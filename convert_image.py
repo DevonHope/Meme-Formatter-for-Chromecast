42578e3f1e087a749bb6ef17c22f74b44c1f9d80
WalterMitty2112 <devonhope20@gmail.com>
2020-07-20 12:04:45 -0400

    adding delete to conver_image

--- convert_image.py
+++ convert_image.py
@@ -1,17 +1,34 @@
 from PIL import Image, ImageDraw, ImageFilter
 from os import listdir 
 from os.path import isfile, join
+from pathlib import Path
 
-bgdir = "bgmemes"
+bgdir = "resources/bgmemes"
 
 #get all background images
 allbg = [im for im in listdir(bgdir) if isfile(join(bgdir, im))]
 
 for bg in allbg:
-	if (bg.format != "JPEG"):
+	p_bg = bgdir+"/"+bg
+	im = Image.open(p_bg)
+	print(bg)
+	print(im.format)
+
+	if (im.format != 'JPEG'):
+		print("\n")
+		print("not jpg")
+		print(bg)
+	"""
 		#convert to jpg
-		p_bg = "bgmemes/"+bg
-		im = Image.open(p_bg)
 		rgb_im = im.convert('RGB')
 		newname = p_bg.partition('.')[0] + ".jpg"
 		rgb_im.save(newname)
+		print("\n")
+		print(bgdir+"/"+bg)
+		#rmfile = Path(str(bgdir+"/"+bg))
+		#rmfile.unlink()
+	else:
+		print("\n")
+		print("jpg")
+		print(bg)
+	"""
--- meme_formatter.py
+++ meme_formatter.py
@@ -10,6 +10,16 @@ newme = "resources/new_memes"
 #get all background images
 allbg = [im for im in listdir(bgdir) if isfile(join(bgdir, im))]
 
+#convert all background images to jpg for RGBA formatting in Pillow
+for bg in allbg:
+	if (bg.format != "JPEG"):
+		#convert to jpg
+		p_bg = bgdir+"/"+bg
+		im = Image.open(p_bg)
+		rgb_im = im.convert('RGB')
+		newname = p_bg.partition('.')[0] + ".jpg"
+		rgb_im.save(newname)
+
 #get all memes
 allmeme = [i for i in listdir(memedir) if isfile(join(memedir, i))]
 
